38681c01227fed07fdbf73422913b77490bc920d
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2023-01-27 17:19:28 +0300

    complete task2

--- task2.java
+++ task2.java
@@ -1,6 +1,32 @@
+import java.util.Arrays;
+
 // Task2
 // Реализуйте алгоритм сортировки пузырьком для сортировки массива
 
 public class task2 {
+    public static void main(String[] args) {
 
+        int n = 21;
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 301);
+        }
+
+        boolean isSorted = false;
+        int buf = 0;
+
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    isSorted = false;
+
+                    buf = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = buf;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
 }
