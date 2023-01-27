ad31ffcfdc3f466aed55a3ba2bd5e9a0c3daae42
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2023-01-27 18:18:10 +0300

    complete task3

--- task3.java
+++ task3.java
@@ -4,5 +4,20 @@
 // оператор "%" (остаток от деления) и циклы.
 
 public class task3 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            boolean isPrime = true;
+            double squareRoot = Math.sqrt(i) + 2;
+            int j = 2;
+            while (j <= squareRoot && isPrime) {
+                if (i == 2 || i == 3)
+                    break;
+                if (i % j == 0)
+                    isPrime = false;
+                j++;
+            }
+            System.out.print(isPrime ? (i + " ") : "");
+        }
+    }
 
 }
